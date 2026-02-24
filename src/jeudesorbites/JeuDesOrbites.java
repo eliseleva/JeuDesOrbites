91f4e8c4aa7d5ac74a4b1992c29728f4288d4c6b
eliseleva <louna.titanium@gmail.com>
2026-02-24 15:03:25 +0100

    blabla

--- src/jeudesorbites/JeuDesOrbites.java
+++ src/jeudesorbites/JeuDesOrbites.java
@@ -11,12 +11,11 @@ import java.util.Random;
 
 public class JeuDesOrbites {
     
-    Random rand = new Random();
-    int choixLettre = rand.nextInt(24);
+  
     
     public static void main(String[] args) {
         
-        Lettre maLettre= new Lettre();
+        Lettre maLettre= new Lettre("",1);
         
         System.out.println("blabla");
         
